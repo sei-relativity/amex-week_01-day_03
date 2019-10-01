8cfd5eba0283512fa21d36ba3f2c257dc46c8d08
mansourSaleh <mansouralmohsen2018@gmai.com>
2019-10-01 10:08:55 +0300

    add solution

--- raindrops.js
+++ raindrops.js
@@ -1,2 +1,18 @@
 // Your Answer below
 
+let output = "";
+let number = 8;
+
+if (number % 3 === 0) {
+    output = output + "Pling ";
+}
+if (number % 5 === 0) {
+    output = output + " Plang "
+}
+if (number % 7 === 0) {
+    output = output + " Plong"
+}
+if (output === "") {
+    output = number.toString();
+}
+console.log(output);
